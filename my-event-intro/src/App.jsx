0cf60bb6789395fea0f39733dce84d5b212f9f08
Tejas Saraf <tejas@thekiranacademy.com>
2026-03-26 17:33:53 +0530

    event-intro

---
+++ my-event-intro/src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+import './App.css'
+
+function App() {
+
+  let name = "Tejas"
+  let count = 0;
+  let [ucount, setUcount] = useState(0)
+  const handleClick = () => {
+    count++;
+    setUcount(ucount + 1)
+    console.log("count = " + count + " --- u count = " + ucount)
+
+  }
+
+  return (
+    <>
+      <center>
+        <h1>Welcome to Event State Intro... Mr. {name}</h1>
+
+        {/* 1st event onCLick .. attributes in react follows thhe camelCase */}
+        <button onClick={handleClick}>Click Me</button>
+        <h2>Normal Var Count = {count}</h2>
+        <h2>useState Var Count = {ucount}</h2>
+      </center>
+    </>
+  )
+}
+
+export default App
