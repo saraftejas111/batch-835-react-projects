45c30fe374f9807da0801abf80b581aa4b137dad
Tejas Saraf <tejas@thekiranacademy.com>
2026-04-03 17:24:38 +0530

    emps added

--- my-revision-spp/src/components/ShowAll.jsx
+++ my-revision-spp/src/components/ShowAll.jsx
@@ -1,23 +1,48 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { getAllEmployees } from '../services'
 
 const ShowAll = () => {
-  return (
-    <div>
-      
-      <h2>All Employees </h2>
 
-      <table border="1">
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>NAME</th>
-                <th>ROLE</th>
-                <th>SALARY</th>
-            </tr>
-        </thead>
-      </table>
-    </div>
-  )
+    let [allemps, setAllEmps] = useState([])
+
+    const loadEmployees = () => {
+        setAllEmps(getAllEmployees)
+    }
+
+    useEffect(() => {
+        loadEmployees();
+    }, [])
+
+    return (
+        <div>
+
+            <h2>All Employees </h2>
+
+            <table border="1">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>NAME</th>
+                        <th>ROLE</th>
+                        <th>SALARY</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    {
+                        allemps.map((e) => (
+                            <tr key={e.id}>
+                                <td>{e.id}</td>
+                                <td>{e.name}</td>
+                                <td>{e.role}</td>
+                                <td>{e.salary}</td>
+                            </tr>
+                        ))
+                    }
+                </tbody>
+            </table>
+        </div>
+    )
 }
 
 export default ShowAll
---
+++ my-revision-spp/src/services.js
@@ -0,0 +1,10 @@
+const employees = [
+  { id: 101, name: "tejas", role: "dev", salary: 1234 },
+  { id: 102, name: "neha", role: "test", salary: 1212 },
+  { id: 103, name: "raj", role: "dev", salary: 12345 },
+  { id: 104, name: "tina", role: "test", salary: 12123 },
+];
+
+
+export const getAllEmployees = () => employees ; 
+
